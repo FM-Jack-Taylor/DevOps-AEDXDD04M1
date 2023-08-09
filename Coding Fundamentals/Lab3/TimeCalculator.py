f78986187e7f02fa760575a2bdd50301259064b7
Admin <Admin@STUDENT05>
2023-08-09 09:42:07 +0100

    update

--- Coding Fundamentals/Lab3/TimeCalculator.py
+++ Coding Fundamentals/Lab3/TimeCalculator.py
@@ -4,14 +4,37 @@ if userop == 1:
     time2 = str(input ("please enter time DD:HH:MM: "))
     time1 = time1.split()
     time2 = time2.split()
-    time1 
+    min1 = int(time1[2])
+    hour1 = int(time1[1])
+    day1 = int(time1[0])
+    min2 = int(time2[2])
+    hour2 = int(time2[1])
+    day2 = int(time2[0])
+    totalmin = min1+min2
+    totalhour = hour1+hour2
+    totalday = day1+day2
 
+    while totalmin > 60:
+        totalhour = totalhour + 1
+        totalmin = totalmin - 60
+
+    while totalhour > 24:
+        totalday = totalday + 1
+        totalhour = totalhour - 24
+
+    if totalhour < 9 and totalmin < 9:
+        print(f"{totalday}:0{totalhour}:0{totalmin}")
+        input
+    if totalhour > 9 and totalmin > 9:
+        print(f"{totalday}:{totalhour}:{totalmin}")
+        input
 
 if userop == 2:
     time1 = str(input ("please enter time DD:HH:MM: "))
     time2 = str(input ("please enter time DD:HH:MM: "))
 if userop == 3:
     time1 = str(input ("please enter time DD: "))
+
 if userop == 4:
     time1 = str(input ("please enter time HH: "))
 if userop == 5:
